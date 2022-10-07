26fbe2ab978bc5c2a203d52e117f89b041f31bbd
Dan Kodin <dan_kodin03@mail.ru>
2022-10-07 09:20:41 +0300

    Рабочая программа

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
+меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись
+исключительно массивами. */
+
+
+void Check(string str) {
+    int n = str.Length;
+    string[] massive = str.Split(new char[] { ',' });
+    for(int i = 0; i < massive.Length; i++) 
+    {
+        if (i == 0) Console.Write("[");
+        if (i < massive.Length - 1) 
+           if (massive[i].Length <= 3) Console.Write("\"" + massive[i] + "\", ");
+        if (i == massive.Length - 1) Console.Write("\"" + massive[i] + "\"]");
+    }
+}
+
+Console.WriteLine("Введите слова или символы через запятую без пробела: ");
+string str = Console.ReadLine();
+Check(str);
+
